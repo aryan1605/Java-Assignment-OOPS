ad1c9c1a500f3959b429a1859e6683dd05767763
Aryan Gupta <aryangupta1605@gmail.com>
2021-12-02 20:45:56 +0530

    Files Added

---
+++ src/Two_Dimensional_Array/Student1.java
@@ -0,0 +1,38 @@
+package Two_Dimensional_Array;
+import java.util.Scanner;
+public class Student1 {
+    public String name ;
+    public int percentage ;
+    Student1(String name ,int percentage)
+    {
+        this.name=name;
+        this.percentage=percentage;
+    }
+    public static void result(Student1 s[])
+    {
+        int index=0;
+        int m = Integer.MIN_VALUE;
+        for(int i=0;i<s.length;i++)
+        {
+            if(s[i].percentage>m) {
+                m = s[i].percentage;
+                index=i;
+            }
+        }
+        System.out.println("Topper is "+s[index].name);
+        System.out.println("percentage of topper is "+s[index].percentage);
+    }
+}
+class TestStudent1
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        Student1 []s = new Student1[3];
+        for (int i=0;i<3;i++)
+        {
+            s[i] = new Student1(sc.next(),sc.nextInt());
+        }
+        Student1.result(s);
+    }
+}
